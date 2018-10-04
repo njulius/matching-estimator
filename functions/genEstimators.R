e8172928196a028a3a013b9337d9cbd22ce9f6c9
njulius <njulius@iastate.edu>
2018-10-04 15:57:49 -0500

    Update genEstimators.R

--- functions/genEstimators.R
+++ functions/genEstimators.R
@@ -9,15 +9,15 @@ genEstimators <- function(augTreatedArm, augControlArm) {
   
   # First, calculate ATET
   # Mean of treated outcomes minus weighted mean of control outcomes
-  atet <- mean(augTreatedArm[,1]) - mean(augControlArm[,1]*augControlArm[,5])
+  atet <- mean(augTreatedArm[,1]) - mean(augControlArm[,1]*augControlArm[,7])
   
   # Then calculate ATEU
   # Mean of weighted treated outcomes minus mean of control outcomes
-  ateu <- mean(augTreatedArm[,1]*augTreatedArm[,5]) - mean(augControlArm[,1])
+  ateu <- mean(augTreatedArm[,1]*augTreatedArm[,7]) - mean(augControlArm[,1])
   
   # Calculate ATE
   # Weighted mean on both sides
-  ate <- (1/n)*sum(augTreatedArm[,1]*(1 + augTreatedArm[,5]) - augControlArm[,1]*(1 + augControlArm[,5]))
+  ate <- (1/n)*sum(augTreatedArm[,1]*(1 + augTreatedArm[,7]) - augControlArm[,1]*(1 + augControlArm[,7]))
   
   # Sanity Checker
   # Calculate ATE as weighted average of ATET and ATEU
