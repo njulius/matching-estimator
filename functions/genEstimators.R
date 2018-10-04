ab2efb5f483d598171d9046e28e3c28b0bee4931
njulius <njulius@iastate.edu>
2018-10-04 15:24:23 -0500

    Create genEstimators.R

---
+++ functions/genEstimators.R
@@ -0,0 +1,30 @@
+# Matching Estimator Implementation
+# Niklaus Julius
+
+genEstimators <- function(augTreatedArm, augControlArm) {
+  
+  n1 <- length(augTreatedArm[,1])
+  n0 <- length(augControlArm[,1])
+  n <- n1 + n0
+  
+  # First, calculate ATET
+  # Mean of treated outcomes minus weighted mean of control outcomes
+  atet <- mean(augTreatedArm[,1]) - mean(augControlArm[,1]*augControlArm[,4])
+  
+  # Then calculate ATEU
+  # Mean of weighted treated outcomes minus mean of control outcomes
+  ateu <- mean(augTreatedArm[,1]*augTreatedArm[,4]) - mean(augControlArm[,1])
+  
+  # Calculate ATE
+  # Weighted mean on both sides
+  ate <- (1/n)*sum(augTreatedArm[,1]*(1 + augTreatedArm[,4]) - augControlArm[,1]*(1 + augControlArm[,4]))
+  
+  # Sanity Checker
+  # Calculate ATE as weighted average of ATET and ATEU
+  sanity <- (n1/n) * atet + (n0/n) * ateu
+  
+  results <- c(atet, ateu, ate, sanity)
+  
+  return(results)
+  
+}
