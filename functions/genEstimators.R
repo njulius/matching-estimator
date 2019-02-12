1592111b5209dee0bf3e3f7adce6c5b295273939
njulius <njulius@iastate.edu>
2019-02-12 14:37:11 -0600

    Update genEstimators.R
    previous version was broken and only worked with equal treatment ratios for a reason I cannot figure out

--- functions/genEstimators.R
+++ functions/genEstimators.R
@@ -1,29 +1,32 @@
 # Matching Estimator Implementation
 # Niklaus Julius
 
-genEstimators <- function(augTreatedArm, augControlArm) {
+getEstimates <- function(augTreatedArm, augControlArm) {
   
   n1 <- length(augTreatedArm[,1])
   n0 <- length(augControlArm[,1])
   n <- n1 + n0
   
-  # First, calculate ATET
-  # Mean of treated outcomes minus weighted mean of control outcomes
-  atet <- mean(augTreatedArm[,1]) - mean(augControlArm[,1]*augControlArm[,7])
+  # Generate ATET
   
-  # Then calculate ATEU
-  # Mean of weighted treated outcomes minus mean of control outcomes
-  ateu <- mean(augTreatedArm[,1]*augTreatedArm[,7]) - mean(augControlArm[,1])
+  atet <- (sum(augTreatedArm[,1]) - sum(augControlArm[,1]*augControlArm[,7]))*(1/n1)
   
-  # Calculate ATE
-  # Weighted mean on both sides
-  ate <- (1/n)*sum(augTreatedArm[,1]*(1 + augTreatedArm[,7]) - augControlArm[,1]*(1 + augControlArm[,7]))
+  # Generate ATEU
   
-  # Sanity Checker
-  # Calculate ATE as weighted average of ATET and ATEU
-  sanity <- (n1/n) * atet + (n0/n) * ateu
+  ateu <- (sum(augTreatedArm[,1]*augTreatedArm[,7]) - sum(augControlArm[,1]))*(1/n0)
+           
+  # Generate ATE
   
-  results <- c(atet, ateu, ate, sanity)
+  treatedIntermediate <- sum(augTreatedArm[,1]) + sum(augTreatedArm[,1]*augTreatedArm[,7])
+  controlIntermediate <- sum(augControlArm[,1]) + sum(augControlArm[,1]*augControlArm[,7])
+  
+  ate <- (1/n)*(treatedIntermediate - controlIntermediate)
+  
+  # Generate Sanity
+  
+  sanity <- (n1/n)*atet + (n0/n)*ateu
+  
+  results <- cbind(atet, ateu, ate, sanity)
   
   return(results)
   
